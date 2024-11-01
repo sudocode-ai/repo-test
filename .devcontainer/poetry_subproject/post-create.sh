2b5c22e1d118944ce99b9007cbbf7a44fd72c724
Randy Song <ssh.randy@gmail.com>
2024-11-01 10:38:47 -0700

    try again

--- .devcontainer/poetry_subproject/post-create.sh
+++ .devcontainer/poetry_subproject/post-create.sh
@@ -8,6 +8,10 @@ source /home/jovyan/poetry_subproject/bin/activate
 echo "Using Python from: $(which python)"
 echo "Python version: $(python --version)"
 
+# Configure poetry to use the current virtual environment
+poetry config virtualenvs.create false --local
+poetry config virtualenvs.in-project true --local
+
 cd poetry_subproject
 pip install pytest
 poetry install --all-extras --with dev --with test || \
