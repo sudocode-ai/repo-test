3ebbc1ad5be2faa56098d914e9bf15d8c2246ad8
Randy Song <ssh.randy@gmail.com>
2024-10-31 22:29:52 -0700

    small fix

--- .devcontainer/poetry_subproject/post-create.sh
+++ .devcontainer/poetry_subproject/post-create.sh
@@ -3,6 +3,7 @@
 
 python -m venv /home/jovyan/poetry_subproject
 source /home/jovyan/poetry_subproject/bin/activate
+cd poetry_subproject
 pip install poetry ipykernel pytest
 poetry config virtualenvs.in-project true --local
 poetry install --all-extras --with dev --with test
