1e7a146438d7fae96b31e6f4976274a75bab0853
Randy Song <ssh.randy@gmail.com>
2024-11-15 14:37:58 -0800

    small change

--- poetry_subproject/poetry_subproject/parser/base_parser.py
+++ poetry_subproject/poetry_subproject/parser/base_parser.py
@@ -1,9 +1,13 @@
 class BaseParser:
     def base_function(): 
+        print("hello world")
+        bla = 5
         return 10
 
 class SpeializedParser:
     def special_function():
+        print("hello world")
+        bla = 5
         return 5
     
     
