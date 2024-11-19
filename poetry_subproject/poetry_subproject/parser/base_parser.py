48588fe9ff37130e795d4a58595f0f0b42817852
Randy Song <ssh.randy@gmail.com>
2024-11-18 22:42:14 -0800

    commit stuff

--- poetry_subproject/poetry_subproject/parser/base_parser.py
+++ poetry_subproject/poetry_subproject/parser/base_parser.py
@@ -2,6 +2,7 @@ class BaseParser:
     def base_function(): 
         print("hello world")
         bla = 5
+        print("For a diff")
         return 10
 
 class SpeializedParser:
@@ -10,4 +11,8 @@ class SpeializedParser:
         bla = 5
         return 5
     
-    
+
+def auxilary_function():
+    padding = 5
+    base = 0
+    return padding + base
