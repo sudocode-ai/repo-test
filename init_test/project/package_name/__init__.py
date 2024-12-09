9f3d9fd269ffb179c7f6e84701a26719957ba818
Randy Song <ssh.randy@gmail.com>
2024-12-09 01:26:15 -0800

    fix import again

--- init_test/project/package_name/__init__.py
+++ init_test/project/package_name/__init__.py
@@ -1,2 +1,1 @@
 from .core import CoreClass
-import project.nested_package
--- init_test/project/package_name/tests/test_package_function.py
+++ init_test/project/package_name/tests/test_package_function.py
@@ -1,5 +1,4 @@
 from ...package_name import core as some_package
-
 def test_unknown():
     number = some_package.package_function()
     assert number == 5
