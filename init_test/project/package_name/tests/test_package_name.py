cd40b3c6132b4d2be73385a6f2017962747e2cc0
Randy Song <ssh.randy@gmail.com>
2024-12-09 16:33:14 -0800

    do direct calls

--- init_test/project/package_name/tests/test_package_name.py
+++ init_test/project/package_name/tests/test_package_name.py
@@ -1,9 +1,9 @@
 from ... import package_name as some_package
 
 def test_unknown():
-    some_class = some_package.CoreClass()
-    assert some_class.do_something() == 6
+    result = some_package.CoreClass().do_something()
+    assert result == 6
 
 def test_second_package():
-    other_class = some_package.nested_package.OtherCoreClass()
-    assert other_class.do_something() == 6
+    result = some_package.nested_package.OtherCoreClass().do_something()
+    assert result == 6
