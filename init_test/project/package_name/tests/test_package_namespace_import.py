504c7a16dd5572877316df323ad443429dd0532a
Randy Song <ssh.randy@gmail.com>
2024-12-10 22:55:20 -0800

    woops

--- init_test/project/package_name/tests/test_package_namespace_import.py
+++ init_test/project/package_name/tests/test_package_namespace_import.py
@@ -1,16 +1,16 @@
-import package_name
+import project.package_name
 
 def test_package_exported_class():
     # Tests CoreClass imported through package __init__.py
-    number = package_name.CoreClass()
+    number = project.package_name.CoreClass()
     assert number == 5
     
 def test_package_exported_nested_class():
     # Tests OtherCoreClass imported through package __init__.py
-    number = package_name.OtherCoreClass()
+    number = project.package_name.OtherCoreClass()
     assert number == 5
     
 def test_package_exported_function():
     # Tests package_function imported through package __init__.py
-    number = package_name.package_function()
+    number = project.package_name.package_function()
     assert number == 5
