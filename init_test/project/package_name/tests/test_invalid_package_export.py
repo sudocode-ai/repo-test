a53b19a1015f9012c9177b83847f26542f3e5a88
Randy Song <ssh.randy@gmail.com>
2024-12-11 01:05:50 -0800

    add new test

---
+++ init_test/project/package_name/tests/test_invalid_package_export.py
@@ -0,0 +1,16 @@
+from ...package_name import CoreClass, OtherCoreClass
+
+def test_package_exported_class():
+    # Tests CoreClass imported through package __init__.py
+    number = CoreClass()
+    assert number == 5
+    
+def test_package_exported_nested_class():
+    # Tests OtherCoreClass imported through package __init__.py
+    number = OtherCoreClass()
+    assert number == 5
+    
+def test_package_exported_function():
+    # Tests package_function imported through package __init__.py
+    number = package_function() #NOTE: This call should NOT resolve
+    assert number == 5
