840b1e5ebfc66194e144b273af8b25aefe0c146e
Randy Song <ssh.randy@gmail.com>
2024-12-12 14:47:39 -0800

    add object tracking test

--- init_test/project/package_name/tests/test_object_call_tracking.py
+++ init_test/project/package_name/tests/test_object_call_tracking.py
@@ -1,10 +1,11 @@
 from ...package_name import DerivedInternalBaseClass, DerivedExternalBaseClass
 
 def test_derived_internal_class():
-    print("Hello")
-    assert DerivedInternalBaseClass().get_number() == 5
+    number = DerivedInternalBaseClass()
+    assert number.get_number() == 5
     
 def test_derived_external_package_class():
-    print("Hello")
-    assert DerivedExternalBaseClass().get_number() == 5
+    number = DerivedExternalBaseClass()
+    assert number.get_number() == 5
+    
     
